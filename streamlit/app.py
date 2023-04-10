fcbd495b1fabcb341636c3ebe1a5d1d4ecdab5cd
Hamza <hamza.lbelghiti@gmail.com>
2023-04-10 00:50:43 +0000

    Streamlit App

---
+++ streamlit/app.py
@@ -0,0 +1,70 @@
+import streamlit as st
+import os
+from gptube import generate_answer, is_valid_openai_key, is_valid_youtube_url, get_video_duration, calculate_api_cost
+
+st.set_page_config(page_title="GPTube", page_icon='🎥')
+
+
+# App UI
+def gptube_app():
+
+    st.header("🎥 GPTube : Your shortcut to video insights")
+
+    # OPENAI API KEY
+    openai_api_key = st.text_input("🔑 Your OPENAI API KEY ([Get yours from the OPENAI website](https://platform.openai.com/account/api-keys)) : ", placeholder="sk-***********************************", type="password")
+    
+    # Disable YouTube URL field until OpenAI API key is valid
+    if openai_api_key:
+        youtube_url = st.text_input("📹 YouTube Video URL : ", placeholder="https://www.youtube.com/watch?v=************")
+    else:
+        youtube_url = st.text_input(
+            "📹 YouTube Video URL : ",
+            placeholder="Please enter a valid OpenAI API key first",
+            disabled=True,
+        )
+
+    if is_valid_youtube_url(youtube_url):
+        video_duration = get_video_duration(youtube_url)
+        api_call_cost = calculate_api_cost(video_duration)
+
+        if video_duration >= 4 and video_duration <= 20:
+            st.success(f"The duration of the video is {video_duration} minutes. This will cost you approximately ${api_call_cost}")
+        else:
+            st.warning("Please enter a youtube video that is 4 minutes long at minimum and 20 minutes at maximum.")
+    else:
+        st.error("Please enter a valid YouTube video URL.")
+
+    if openai_api_key and youtube_url:
+        question = st.text_input("🤔 What are you looking for ?", placeholder="What does X mean ? How to do X ?")
+    else:
+        question = st.text_input("🤔 What are you looking for ?", placeholder="Please enter a valid OpenAI API and YouTube URL key first", disabled=True)
+        
+    # Add a button to run the function
+    if st.button("Generate Answer"):
+        if not is_valid_openai_key(openai_api_key):
+            st.markdown(f"##### ❌ Please enter a valid OpenAI API key. ")
+
+        elif not youtube_url:
+            st.warning("Please enter the YouTube video URL.")
+        elif not question:
+            st.warning("Please enter your question.")
+        else:
+            os.environ["OPENAI_API_KEY"] = openai_api_key
+
+            with st.spinner("Generating answer..."):
+                # Call the function with the user inputs
+                answer = generate_answer(openai_api_key, youtube_url, question)
+
+            st.markdown(f"#### {question}")
+            st.success(answer)
+            
+gptube_app()
+
+# Hide Left Menu
+st.markdown("""<style>
+#MainMenu {visibility: hidden;}
+footer {visibility: hidden;}
+</style>""", unsafe_allow_html=True)
+
+
+
---
+++ streamlit/gptube.py
@@ -0,0 +1,102 @@
+import os 
+import re
+import openai
+import requests
+import tempfile
+from pytube import YouTube
+from urllib.parse import urlparse, parse_qs
+
+from langchain.indexes import VectorstoreIndexCreator
+from langchain.document_loaders import TextLoader
+
+# Validation Functions
+def is_valid_openai_key(api_key) -> bool:
+    try:
+        # Test OpenAI API key by making a request to the authentication endpoint
+        auth_response = requests.get("https://api.openai.com/v1/engines", headers={"Authorization": f"Bearer {api_key}"})
+        auth_response.raise_for_status()
+
+        # Test OpenAI Python package by creating an instance of the openai.api object
+        openai.api_key = api_key
+        openai.Completion.create(engine="text-davinci-002", prompt="Hello, World!")
+
+        # If both tests pass, the API key is valid
+        return True
+
+    except (requests.exceptions.HTTPError, Exception):
+        # If either test fails, the API key is invalid
+        return False
+    
+def is_valid_youtube_url(url: str) -> bool:
+    pattern = r'^https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(\&ab_channel=[\w\d]+)?$'
+    match = re.match(pattern, url)
+    
+    return match is not None
+
+# Calculate YouTube video duration
+def get_video_duration(url: str) -> float:
+    yt = YouTube(url)  
+    video_length = round(yt.length / 60, 2)
+
+    return video_length
+
+# Calculate API call cost
+def calculate_api_cost(video_length: float) -> float:
+    api_call_cost = round(video_length * 0.006, 2)
+
+    return api_call_cost
+
+# Transcription 
+def transcribe_audio(file_path):
+        # Get the size of the file in bytes
+        file_size = os.path.getsize(file_path)
+
+        # Convert bytes to megabytes
+        file_size_in_mb = file_size / (1024 * 1024)
+
+        # Check if the file size is less than 25 MB
+        if file_size_in_mb < 25:
+            with open(file_path, "rb") as audio_file:
+                transcript = openai.Audio.transcribe("whisper-1", audio_file)
+
+            return transcript
+        else:
+            print("Please provide a smaller audio file (less than 25mb).")
+
+# Generate Answer
+def generate_answer(api_key: str, url: str, question: str) -> str:
+
+    openai.api_key = api_key
+
+    # Extract the video_id from the url
+    query = urlparse(url).query
+    params = parse_qs(query)
+    video_id = params["v"][0]
+
+    with tempfile.TemporaryDirectory() as temp_dir:
+        # The path of the downloaded mp3
+        audio_filepath = f"{temp_dir}/{video_id}.mp3"
+
+        # Downloading the youtube video as mp3 using yt-dlp
+        os.system(f"yt-dlp -x --audio-format mp3 --output {temp_dir}/{video_id} https://www.youtube.com/watch?v={video_id}")
+
+        # The path of the transcript
+        transcript_filepath = f"{temp_dir}/{video_id}.txt"
+
+        # Transcribe the mp3 audio to text
+        transcript = transcribe_audio(audio_filepath)
+        
+        # Writing the content of transcript into a txt file
+        with open(transcript_filepath, 'w') as transcript_file:
+            transcript_file.write(transcript['text'])
+
+        # Loading the transcript file
+        loader = TextLoader(transcript_filepath)
+
+        # Create index
+        index = VectorstoreIndexCreator().from_loaders([loader])
+
+        # Answer the question
+        answer = index.query(question)
+
+    return answer.strip()
