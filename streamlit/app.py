41bbb32788b62ab757f1be77cd94baae6b8b33dc
Hamza <hamza.lbelghiti@gmail.com>
2023-05-14 00:08:33 +0100

    Switched to gpt-3.5-turbo llm & changed app layout

--- streamlit/app.py
+++ streamlit/app.py
@@ -1,61 +1,95 @@
 import streamlit as st
 import os
 
-from gptube import generate_answer, generate_summary, is_valid_openai_key, is_valid_youtube_url, get_video_duration, calculate_api_cost
+from gptube import generate_answer, generate_summary, video_info, is_valid_openai_key, is_valid_youtube_url, get_video_duration, calculate_api_cost
 
 st.set_page_config(page_title="GPTube", page_icon='🎥')
 
 # App UI
 def gptube_app():
 
-    st.header("🎥 GPTube : Your shortcut to video insights")
+    with st.sidebar:
+        st.markdown("### 🎥 GPTube : Video Demo")
+
+        st.video("https://youtu.be/vqThQVTgAp8")
+
+        st.markdown("""<div style="text-align: justify;">Have you ever found yourself going through a long YouTube video, trying to find the answer to a specific question? With GPTube, 
+                    you can simply ask the question you want to find the answer to, and in less than 2 minutes, 
+                    you can get the answer at a low cost of only $0.006 per minute of video. Or get a summary of the entire video
+                    for just $0.009/minute.</div>""", unsafe_allow_html=True)
+        
+        st.markdown("#")
+
+        st.markdown("😼 Source code on [GitHub](https://github.com/Hamagistral/GPTube)")
+        st.markdown("🛠️ Made by [Hamza El Belghiti](https://www.linkedin.com/in/hamza-elbelghiti/)")
+
+
+    st.header("🎥 GPTube : Your Shortcut to Video Insights")
+
+    st.markdown('#') 
+
+    choice = st.radio("Please choose an option :", ('Generate Summary', 'Generate Answer to a Question'), horizontal=True)
 
     st.markdown('#') 
 
     # OPENAI API KEY
-    openai_api_key = st.text_input("🔑 Your OpenAI API KEY ([Get yours from the OPENAI website](https://platform.openai.com/account/api-keys)) : ", placeholder="sk-***********************************", type="password")
+    st.markdown('#### 🔑 Step 1 : Enter Your OpenAI API Key') 
+    openai_api_key = st.text_input("[Get Yours From the OPENAI Website](https://platform.openai.com/account/api-keys) : ", placeholder="sk-***********************************", type="password")
     
     # Disable YouTube URL field until OpenAI API key is valid
     if openai_api_key:
-        youtube_url = st.text_input("📹 YouTube Video URL : ", placeholder="https://www.youtube.com/watch?v=************")
+        st.markdown('#### 📹 Step 2 : Enter the YouTube Video URL')
+        youtube_url = st.text_input("URL :", placeholder="https://www.youtube.com/watch?v=************")
     else:
+        st.markdown('#### 📹 Step 2 : Enter the YouTube Video URL')
         youtube_url = st.text_input(
-            "📹 YouTube Video URL : ",
+            "URL : ",
             placeholder="Please enter a valid OpenAI API key first",
             disabled=True
         )
 
     if is_valid_youtube_url(youtube_url):
         video_duration = get_video_duration(youtube_url)
-        api_call_cost = calculate_api_cost(video_duration)
+        option = 'summary' if choice == 'Generate Summary' else 'answer'
+        api_call_cost = calculate_api_cost(video_duration, option)
 
         if video_duration >= 4 and video_duration <= 20:
             st.info(f"The duration of the video is {video_duration} minutes. This will cost you approximately ${api_call_cost}")
+            
+            thumbnail_url, video_title = video_info(youtube_url)
+            st.markdown(f"#### {video_title}")
+            st.image(f"{thumbnail_url}", use_column_width='always')
+            
         else:
             st.warning("Please enter a youtube video that is 4 minutes long at minimum and 20 minutes at maximum.")
     else:
         st.error("Please enter a valid YouTube video URL.")
 
-    st.markdown('#') 
 
-    choice = st.radio("Please choose an option before inputing any information :", ('Generate Summary', 'Generate Answer to a Question'), horizontal=True)
-
-    if choice == 'Generate Summary':
+    if choice == "Generate Summary":
         if openai_api_key and youtube_url:
-            with st.spinner("Generating answer..."):
-                    # Call the function with the user inputs
-                    summary = generate_summary(openai_api_key, youtube_url)
+            if st.button("Generate Summary"):
+                if not is_valid_openai_key(openai_api_key):
+                    st.markdown(f"##### ❌ Please enter a valid OpenAI API key. ")
+                elif not youtube_url:
+                    st.warning("Please enter the YouTube video URL.")
+                else:
+                    with st.spinner("Generating summary..."):
+                            # Call the function with the user inputs
+                            summary = generate_summary(openai_api_key, youtube_url)
 
-            st.markdown(f"#### Summary :")
-            st.success(summary)
+                    st.markdown(f"#### 🤖 Summary :")
+                    st.success(summary)
         else:
             st.warning("Please enter a valid OpenAI API and YouTube URL key first")
 
-    else: 
+    elif choice == "Generate Answer to a Question": 
         if openai_api_key and youtube_url:
-            question = st.text_input("🤔 What are you looking for ?", placeholder="What does X mean ? How to do X ?")
+            st.markdown('#### 🤔 Step 3 : Enter your question')
+            question = st.text_input("What are you looking for ?", placeholder="What does X mean ? How to do X ?")
         else:
-            question = st.text_input("🤔 What are you looking for ?", placeholder="Please enter a valid OpenAI API and YouTube URL key first", disabled=True)
+            st.markdown('#### 🤔 Step 3 : Enter your question')
+            question = st.text_input("What are you looking for ?", placeholder="Please enter a valid OpenAI API and YouTube URL key first", disabled=True)
             
         # Add a button to run the function
         if st.button("Generate Answer"):
@@ -73,7 +107,7 @@ def gptube_app():
                     # Call the function with the user inputs
                     answer = generate_answer(openai_api_key, youtube_url, question)
 
-                st.markdown(f"#### {question}")
+                st.markdown(f"#### 🤖 {question}")
                 st.success(answer)
             
 gptube_app()
--- streamlit/gptube.py
+++ streamlit/gptube.py
@@ -3,15 +3,18 @@ import re
 import openai
 import requests
 
+from pytube.exceptions import VideoUnavailable
+from urllib.parse import urlparse, parse_qs
 from moviepy.editor import *
 from pytube import YouTube
-from urllib.parse import urlparse, parse_qs
 
 from langchain.chains.summarize import load_summarize_chain
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.indexes import VectorstoreIndexCreator
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.document_loaders import TextLoader
 from langchain.docstore.document import Document
+from langchain.vectorstores import Chroma
+from langchain.chains import RetrievalQA
 from langchain.llms import OpenAI
 
 # Validation Functions
@@ -33,10 +36,20 @@ def is_valid_openai_key(api_key) -> bool:
         return False
     
 def is_valid_youtube_url(url: str) -> bool:
-    pattern = r'^https:\/\/www\.youtube\.com\/watch\?v=([a-zA-Z0-9_-]+)(\&ab_channel=[\w\d]+)?$'
-    match = re.match(pattern, url)
-    
-    return match is not None
+    # Check if the URL is a valid YouTube video URL
+    try:
+        # Create a YouTube object
+        yt = YouTube(url)
+
+        # Check if the video is available
+        if not yt.video_id:
+            return False
+
+    except (VideoUnavailable, Exception):
+        return False
+
+    # Return True if the video is available
+    return yt.streams.filter(adaptive=True).first() is not None
 
 # Calculate YouTube video duration
 def get_video_duration(url: str) -> float:
@@ -46,10 +59,24 @@ def get_video_duration(url: str) -> float:
     return video_length
 
 # Calculate API call cost
-def calculate_api_cost(video_length: float) -> float:
-    api_call_cost = round(video_length * 0.006, 2)
+def calculate_api_cost(video_length: float, option: str) -> float:
+    if option == 'summary':
+        api_call_cost = round(video_length * 0.009, 2)
+    elif option == 'answer':
+        api_call_cost = round(video_length * 0.006, 2)
 
     return api_call_cost
+
+# Get Video Thumbnail URL & Title
+def video_info(url: str):
+
+    yt = YouTube(url)
+
+    # Get the thumbnail URL and title
+    thumbnail_url = yt.thumbnail_url
+    title = yt.title
+
+    return thumbnail_url, title
 
 # Download YouTube video as Audio
 def download_audio(url: str):
@@ -93,6 +120,9 @@ def transcribe_audio(file_path, video_id):
                 with open(transcript_filepath, 'w') as transcript_file:
                     transcript_file.write(transcript['text'])
 
+            # Deleting the mp3 file
+            os.remove(file_path)
+
         else:
             print("Please provide a smaller audio file (less than 25mb).")
 
@@ -100,6 +130,9 @@ def transcribe_audio(file_path, video_id):
 def generate_answer(api_key: str, url: str, question: str) -> str:
 
     openai.api_key = api_key
+
+    llm = OpenAI(temperature=0, openai_api_key=api_key, model_name="gpt-3.5-turbo")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
 
     # Extract the video_id from the url
     query = urlparse(url).query
@@ -112,16 +145,19 @@ def generate_answer(api_key: str, url: str, question: str) -> str:
     # The path of the transcript
     transcript_filepath = f"tmp/{video_id}.txt"
 
-    # Check if the audio file and transcript file already exist
-    if os.path.exists(audio_path) and os.path.exists(transcript_filepath):
-        # Loading the transcript file
-        loader = TextLoader(transcript_filepath)
+    # Check if the transcript file already exist
+    if os.path.exists(transcript_filepath):
+        
+        loader = TextLoader(transcript_filepath, encoding='utf8')
+        documents = loader.load()
+        
+        texts = text_splitter.split_documents(documents)
+        embeddings = OpenAIEmbeddings()
+        db = Chroma.from_documents(texts, embeddings)
 
-        # Create index
-        index = VectorstoreIndexCreator().from_loaders([loader])
-
-        # Answer the question
-        answer = index.query(question)
+        retriever = db.as_retriever()
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+        answer = qa.run(question)
 
     else: 
         download_audio(url)
@@ -129,15 +165,18 @@ def generate_answer(api_key: str, url: str, question: str) -> str:
         # Transcribe the mp3 audio to text
         transcribe_audio(audio_path, video_id)
 
-        # Loading the transcript file
-        loader = TextLoader(transcript_filepath)
-
-        # Create index
-        index = VectorstoreIndexCreator().from_loaders([loader])
-
-        # Answer the question
-        answer = index.query(question)
+        # Generating summary of the text file
+        loader = TextLoader(transcript_filepath, encoding='utf8')
+        documents = loader.load()
         
+        texts = text_splitter.split_documents(documents)
+        embeddings = OpenAIEmbeddings()
+        db = Chroma.from_documents(texts, embeddings)
+
+        retriever = db.as_retriever()
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+        answer = qa.run(question)
+
     return answer.strip()
     
 # Generating Video Summary 
@@ -145,7 +184,7 @@ def generate_summary(api_key: str, url: str) -> str:
 
     openai.api_key = api_key
 
-    llm = OpenAI(temperature=0, openai_api_key=api_key)
+    llm = OpenAI(temperature=0, openai_api_key=api_key, model_name="gpt-3.5-turbo")
     text_splitter = CharacterTextSplitter()
 
     # Extract the video_id from the url
@@ -159,8 +198,8 @@ def generate_summary(api_key: str, url: str) -> str:
     # The path of the transcript
     transcript_filepath = f"tmp/{video_id}.txt"
 
-    # Check if the audio file and transcript file already exist
-    if os.path.exists(audio_path) and os.path.exists(transcript_filepath):
+    # Check if the transcript file already exist
+    if os.path.exists(transcript_filepath):
         # Generating summary of the text file
         with open(transcript_filepath) as f:
             transcript_file = f.read()
