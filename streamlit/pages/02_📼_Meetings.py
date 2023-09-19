fbf587d0dea5916cad0c5c74f889b16393e138f4
Hamza <hamza.lbelghiti@gmail.com>
2023-09-19 22:47:41 +0100

    changed audio path

--- streamlit/pages/02_📼_Meetings.py
+++ streamlit/pages/02_📼_Meetings.py
@@ -8,20 +8,17 @@ st.set_page_config(page_title="GPTMeeting", page_icon='📼')
 
 aai.settings.api_key = st.secrets["ASSEMBLYAI_API_KEY"] 
 
-# Create the 'tmp' directory if it doesn't exist
-os.makedirs('tmp', exist_ok=True)
-
 # Text-to-speech (TTS) for the summary
 engine = pyttsx3.init()
 voices = engine.getProperty('voices')
 engine.setProperty('voice', voices[1].id)
 engine.setProperty('rate', 175)
-engine.save_to_file("Hello", "tmp/summary.mp3")
+engine.save_to_file("Hello", "summary.mp3")
 engine.runAndWait()
 
 # Display the audio summary
 st.markdown("#### 🔊 Audio Summary:")
-st.audio("tmp/summary.mp3")
+st.audio("summary.mp3")
 
 def summarize_meeting(audio_file):
 
