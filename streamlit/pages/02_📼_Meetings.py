928fba00266cd5ec9af85bc26bca21200b8fad81
Hamza <hamza.lbelghiti@gmail.com>
2023-09-19 22:37:31 +0100

    changed audio path

--- streamlit/pages/02_📼_Meetings.py
+++ streamlit/pages/02_📼_Meetings.py
@@ -71,19 +71,19 @@ def meeting_app():
             st.success(summary)
 
             # Create the 'tmp' directory if it doesn't exist
-            os.makedirs('tmp', exist_ok=True)
+            os.makedirs('streamlit/tmp', exist_ok=True)
 
             # Text-to-speech (TTS) for the summary
             engine = pyttsx3.init()
             voices = engine.getProperty('voices')
             engine.setProperty('voice', voices[1].id)
             engine.setProperty('rate', 175)
-            engine.save_to_file(summary, "tmp/summary.mp3")
+            engine.save_to_file(summary, "streamlit/tmp/summary.mp3")
             engine.runAndWait()
 
             # Display the audio summary
             st.markdown("#### 🔊 Audio Summary:")
-            st.audio("tmp/summary.mp3")
+            st.audio("streamlit/tmp/summary.mp3")
         
 
 meeting_app()
